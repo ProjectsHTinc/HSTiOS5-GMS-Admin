bc326392b6922773ee680a7327b67b674d620218
HappySanzTech <admin@palprotech.com>
2020-06-27 16:01:50 +0530

    Initial Commit

---
+++ GMS Admin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GMS Admin
+//
+//  Created by Happy Sanz Tech on 27/06/20.
+//  Copyright © 2020 HappySanzTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
